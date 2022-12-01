1b9e6939a8b878e2e5c1b9bf0f01fdc4d867a6c3
Klemens <wisserklemens@gmail.com>
2022-12-01 16:11:34 +0100

    Finished Task 5

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -1,18 +1,28 @@
+/**
+ * @author Klemens
+ * @version 1.0
+ */
+
 package de.hfu;
 
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
- */
+* This is my solution for Open Source based Software Development - Task 5
+*/
 public class App 
 {
+	/**
+	 * 
+	 * @param args The Parameter which is given by the user executing the application in Terminal.
+	 */
+	
     public static void main( String[] args )
     {
         System.out.println( "Bitte Zeichenkette eingeben." );
         Scanner sc = new Scanner(System.in);
         String eingabe = sc.next();
+        sc.close();
         eingabe = eingabe.toUpperCase();
         System.out.println(eingabe);
     }
